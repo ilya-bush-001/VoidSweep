801b7711fedc25227771e3f783ab07f22c0961e5
FarmMan <kusilla16@gmail.com>
2025-08-09 08:34:08 +0300

    commit-v.0.0.1

---
+++ src/main/java/net/voidsweep/VoidSweep.java
@@ -0,0 +1,17 @@
+package net.voidsweep;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoidSweep extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

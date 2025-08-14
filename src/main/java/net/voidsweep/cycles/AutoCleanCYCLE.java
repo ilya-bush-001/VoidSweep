cb5a8981f3b0879866246f6a43839bdb82004895
FarmMan <kusilla16@gmail.com>
2025-08-14 12:09:45 +0300

    (6) commit-v0.0.1 | Updated processing of color messages

--- src/main/java/net/voidsweep/cycles/AutoCleanCYCLE.java
+++ src/main/java/net/voidsweep/cycles/AutoCleanCYCLE.java
@@ -1,6 +1,7 @@
 package net.voidsweep.cycles;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
@@ -16,7 +17,7 @@ public class AutoCleanCYCLE {
                 }
             }
         }
-        Bukkit.broadcastMessage("§e" + removed + " §aitems have been cleaned.");
+        Bukkit.broadcastMessage(ChatColor.YELLOW + String.valueOf(removed) + ChatColor.GREEN + " items have been cleaned.");
         return removed;
     }
 }
